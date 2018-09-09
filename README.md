af41dbdcebaab9f94ea23ab080cd342773ff4e9c
Shailendra <regmishailendra1@gmail.com>
2018-09-09 14:42:42 +0545

    added itext with notice and licence

---
+++ README.md
@@ -0,0 +1,39 @@
+
+To use this software you need android studio. I built this software in android studio
+at "https://developer.android.com/studio/"
+
+You need to install Jdk 7 or more or tick option of use embedded jdk.
+After it is installed clone this repo to your studio from the options shown in android studio.
+
+Great!!!
+You installed the project perfectly.
+Press green run button in android studio to run the program.
+
+All the SDK and dependencies comes with this software with exact version.
+Please check build.gradle file in root folder and build.gradle file in root/app.
+
+You are good to go.
+
+This software uses iText licenced under AGPL.
+
+If you look into the source code of iText, you'll see that most files contain a header referring to the AGPL.
+
+"This program is free software; you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation with the addition of the following permission added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY ITEXT GROUP NV, ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program; if not, see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA, or download the license from the following URL: http://itextpdf.com/terms-of-use/
+
+The interactive user interfaces in modified source and object code versions of this program must display Appropriate Legal Notices, as required under Section 5 of the GNU Affero General Public License.
+
+In accordance with Section 7(b) of the GNU Affero General Public License, you must retain the producer line in every PDF that is created or manipulated using iText.
+
+You can be released from the requirements of the license by purchasing a commercial license. Buying such a license is mandatory as soon as you develop commercial activities involving the iText software without disclosing the source code of your own applications. These activities include: offering paid services to customers as an ASP, serving PDFs on the fly in a web application, shipping iText with a closed source product."
+
+
+Full text of the AGPL - GNU AFFERO GENERAL PUBLIC LICENSE
+Version 3, 19 November 2007
+Copyright (C) 2007 Free Software Foundation, Inc.
+Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+
+Check this link for more -
+https://itextpdf.com/AGPL
+
--- app/src/main/java/pdftoword/pdfeditor/edit/pdf/fragment/AboutDialogFragment.java
+++ app/src/main/java/pdftoword/pdfeditor/edit/pdf/fragment/AboutDialogFragment.java
@@ -19,8 +19,44 @@ import pdftoword.pdfeditor.edit.pdf.R;
 
 public class AboutDialogFragment extends DialogFragment {
 
-private String HTML_TEXT="" +
-        "";
+private String HTML_TEXT="<html>  \n" +
+        "                <body>  \n" +
+
+        "Please visit this link <br/>\"https://github.com/regmishailendra/<br/>pdf-editor\" <br/>for source code."
+        +
+        "This software uses iText licenced under AGPL.\n" +
+        "\n" +
+        "If you look into the source code of iText, you'll see that most files contain a header referring to the AGPL and this applies to this program as well.\n" +
+        "\n" +
+        "\"This program is free software; you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation with the addition of the following permission added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY ITEXT GROUP NV, ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS\n" +
+        "\n" +
+        "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program; if not, see <br/>" +
+        "<br/> http://www.gnu.org/licenses/<br/><br/> or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA, or download the license from the following URL: http://itextpdf.com/terms-of-use/\n" +
+        "\n" +
+        "The interactive user interfaces in modified source and object code versions of this program must display Appropriate Legal Notices, as required under Section 5 of the GNU Affero General Public License.\n" +
+        "\n" +
+        "In accordance with Section 7(b) of the GNU Affero General Public License, you must retain the producer line in every PDF that is created or manipulated using iText.\n" +
+        "\n" +
+        "You can be released from the requirements of the license by purchasing a commercial license. Buying such a license is mandatory as soon as you develop commercial activities involving the iText software without disclosing the source code of your own applications. These activities include: offering paid services to customers as an ASP, serving PDFs on the fly in a web application, shipping iText with a closed source product.\"\n" +
+        "\n" +
+        "\n" +
+        "Full text of the AGPL - GNU AFFERO GENERAL PUBLIC LICENSE\n" +
+        "Version 3, 19 November 2007\n" +
+        "Copyright (C) 2007 Free Software Foundation, Inc.\n" +
+        "Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.\n" +
+        "\n" +
+        "Check this link for more -\n" +
+        "<br/>https://itextpdf.com/AGPL<br/>"
+        +
+
+        "                 This app lets you to make your task easier for extracting any text form pdf and lets you edit those words. After we extract text from pdf, we let you to edit those words. After converting those words you can save those to file. After that we fetch those files from storage and let you edit those files again. We do not take any ownership of use/reuse of any files you use here. We take file storage permission with you so that we can save any text you edited here. Also the storage permission is required because we let you to view the files you edited. We also use third party ad networks here to show you ad and those network use the data according to their way to show you the relevant ads. We use internet permissions.\n" +
+        "                  \n" +
+        "                  \n" +
+        "                      \n" +
+        "                  \n" +
+        "                </body>  \n" +
+        "                  \n" +
+        "                </html>";
 
   private Button btn;
 
